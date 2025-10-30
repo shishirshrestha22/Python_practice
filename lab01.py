f1bf7f2a1182e4eb628829d67163b4bbaba429fb
Shishir Shrestha <shishirst71@gmail.com>
2025-10-30 20:38:40 +0545

    Update lab01.py

--- lab01.py
+++ lab01.py
@@ -10,15 +10,19 @@ root1=(-B+d)/(2*A)
 root2=(-B-d)/(2*A)
 print("the first root is: ",root1)
 print("the second root is :",root2)
+# Sample output
 
 # WHEN A = 1 , B=0 ,C = -4
    # root1 = 2.0
    #root2 = -2.0
+
 #WHEN A=1 ,B=5,C=-36
    #root1 = 4.0 & root2 = -9.0
+
 #WHEN A=2 ,B=7.5,C=6
   #root1= -1.1569296691827464
   #root2= -2.5930703308172536
 
-  #WHEN A=0 ,B=3.5,C=8
+#WHEN A=0 ,B=3.5,C=8
+
    # THIS GENERATES ERROR SINCE THE DENOMINATOR BECOMES 0 IN root1 & root2
