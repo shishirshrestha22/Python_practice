aa304d1d0d4862f3f458febbde858b8d4597f68c
Shishir Shrestha <shishirst71@gmail.com>
2025-11-04 21:49:09 +0545

    slight changes

--- numguess.py
+++ numguess.py
@@ -4,17 +4,19 @@ i=0
 while i !=5:
    i=i+1
 print("WELCOME TO NUMBER GUESSSING GAME😊")
+print("Guess the number between 1 to 100")
 b=0
 attempt=0
 while (b!=a):
  attempt=attempt+1
  b=int(input("enter  your guess:"))
  if b==a:
-    print(" congrats!!,correct guess")
+    print(" congrats!!,you made a correct guess")
  elif b>a:
-    print("high guess")
+    print(" your guess is high ")
  elif b<a:
-    print("low guess")
+    print("your guess is low ")
  
 print("the correct number is:",a)
 print("you made correct guess in",attempt,"times")
+
