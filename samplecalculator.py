7215a11edf33dc4575890a2ffc0794f7075b9364
Shishir Shrestha <shishirst71@gmail.com>
2025-11-20 14:34:07 +0545

    function call added

--- samplecalculator.py
+++ samplecalculator.py
@@ -1,14 +1,25 @@
 # SAMPLE CALCULTOR  FOR TWO DIGITS
-userinput1 = int(input("input number 1:"))
-userinput2 = int(input ("input number 2:"))
+def addition (userinput1,userinput2):
+    return (userinput1+userinput2)
+def product (userinput1,userinput2):
+    return (userinput1*userinput2)
+def subtraction (userinput1,userinput2):
+    return (userinput1-userinput2)
+def division(userinput1,userinput2):
+    return (userinput1/userinput2)
+userinput1 = float(input("input number 1:"))
 userinput3 = input("Select an operator(* or + or - or /)")
+userinput2 = float(input ("input number 2:"))
 if userinput3 == "*":
-    print(f"Multiplying {userinput1} by {userinput2} = ",userinput1*userinput2)
+    print(f"Multiplying {userinput1} by {userinput2} = ",product(userinput1,userinput2))
 elif userinput3 == "+":
-    print(f"Adding {userinput1} with {userinput2} = ",userinput1+userinput2)
+    print(f"Adding {userinput1} with {userinput2} = ",addition(userinput1,userinput2))
 elif userinput3 == "-":
-    print(f"Subtracitng {userinput2} from {userinput1} = ",userinput1-userinput2)
+    print(f"Subtracting {userinput2} from {userinput1} = ",subtraction(userinput1,userinput2))
 elif userinput3 == "/":
-    print(f"Dividing {userinput1} by {userinput2} = ",userinput1/userinput2)
+    if userinput2 == 0:
+        print("Dividing by 0 is not possible")
+    else:
+     print(f"Dividing {userinput1} by {userinput2} = ",division(userinput1,userinput2))
 else:
     print("please enter valid operator and try again!")
