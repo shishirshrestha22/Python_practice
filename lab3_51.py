d889e5931157435a3c6c94dd70ef0515425d831f
Shishir Shrestha <shishirst71@gmail.com>
2025-12-06 11:54:31 +0545

    moved lab3 files to week 3

--- lab3_51.py
+++
@@ -1,7 +0,0 @@
-# PROGRAM THAT SUMS THE SQUARES UPTO USER INPUT
-num=int(input("Enter the number: "))
-total=0
-for i in range (1,num+1):
-    total = total + i*i
-
-print("The total sum of squares is :",total)
--- lab3_53.py
+++
@@ -1,9 +0,0 @@
-#PROGRAM TO COMPUTE SUM OF ODD DIGITS IN NON NEGATIVE INTEGER
-num=int(input("Enter the non negative integer:"))
-oddsum = 0
-while num > 0:
-    a = num % 10  #TO EXTRACT THE DIGITS FROM THE NUMBER
-    if a % 2 == 1: #To check the digit is odd or not
-       oddsum = oddsum + a
-    num =  num //10  # this is floor division which removes the last digit so that the loop runs for each digits
-print("the sum of odd digits in the number is ",oddsum)
