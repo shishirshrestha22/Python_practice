b8d0ff635e7f135f2c339b64a50b4fc6590e7589
Shishir Shrestha <shishirst71@gmail.com>
2025-12-14 21:29:48 +0545

    dictuonary methods

---
+++ dict_del_method.py
@@ -0,0 +1,11 @@
+# the del can remove the item with specified key name
+
+mydic = {
+    "brand":"ford",
+    "model":"mustang",
+    "year":1964
+}
+del mydic["model"]
+print(mydic)
+
+# this del can also delete keyword completly
---
+++ dict_practice.py
@@ -0,0 +1,7 @@
+# dictionary pop() method
+
+mydict = {"brand":"ford",
+          "model":"musatang",
+          "year":"1980"}
+mydict.pop("year")
+print(mydict)
