7e3c94e2768a5e5944a39da501c6f846d15c7343
Shishir Shrestha <shishirst71@gmail.com>
2025-12-19 19:18:36 +0545

    optimized the accessibility

--- userinput_in_dict.py
+++ userinput_in_dict.py
@@ -10,3 +10,12 @@ while True:
         break
     students_marks[key] = value
 print("The students and their respective marks are: ",students_marks)
+while True:
+    key = input("Enter the student name to see the marks: ",)
+    if key == "q":
+        break
+    if key in students_marks:
+        print(f"{key} has got {students_marks[key]} marks")
+    else:
+        print("Student not found !")
+print("\n")
