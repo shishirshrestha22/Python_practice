375977f2e221ace81007e58a83fc867feab78008
Shishir Shrestha <shishirst71@gmail.com>
2025-12-23 22:09:30 +0545

    conceptual codes

---
+++ trickyconcepts/concept7.py
@@ -0,0 +1,24 @@
+s = 0
+for i in range (-3,3):
+    if i:
+        s+=1
+print(s)
+
+
+
+
+
+
+# output is 5
+
+""" this code gives output as 5 because it operates from -3,-2,-1,0,1,2 
+and (if i:)stastement  is true for all integer except 0 so"""
+
+#when i = -3 s is 1
+#when i = -2 s becomes 2
+#when i = -1 s becomes 3
+#when i = 0 s remains 3 because if i is false at 0
+#when i = 1 s becomes 4
+#when i = 2 s becomes 5
+#and finally prints 5
+
---
+++ trickyconcepts/concept_6.py
@@ -0,0 +1,7 @@
+
+print(print("python"))
+
+# This prints 
+#python
+#None 
+"""It is because the inner print gives "python" and the outer print return "None" """
