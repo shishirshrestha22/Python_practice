1c0588ea81bea9bdff6a25699e3d48b4b5acb089
Shishir Shrestha <shishirst71@gmail.com>
2026-02-20 22:58:42 +0545

    input funcn added

--- playwright_demo.py
+++ playwright_demo.py
@@ -5,8 +5,9 @@ from playwright.sync_api import sync_playwright#--------------->>imports the pal
 with sync_playwright() as p:#----------------------------------->># Start Playwright and open a browser session
 # 'p' is the Playwright controller
     browser = p.chromium.launch(headless=False)#--------------->>> launch chromuim browser and  headless = False means it shows the window ie windows willl be visible
+    website_name = input("Paste the website link here:")
     page = browser.new_page()# ------------------>> Open a new page (tab) in the browser
-    page.goto("https://www.youtube.com/")#
+    page.goto(website_name)
     print(page.title())#------------------------------------->> prints the title oof the curent page
     input("Press Enter to close the browser...") #----------------->>> keeps browesr opens
     browser.close()#-------------------------------->> close the browser tab
